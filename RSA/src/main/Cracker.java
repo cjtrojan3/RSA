b58fba66d372c4fffcb93c2845b9ce552f21e470
cjtrojan3 <cjtrojan3@gmail.com>
2017-09-10 14:24:28 -0500

    Cracker Added
    Got the cracker created to decode Stevenson's message.

---
+++ RSA/src/main/Cracker.java
@@ -0,0 +1,56 @@
+package main;
+import java.math.*;
+import java.util.ArrayList;
+
+public class Cracker {
+
+	public static void main(String[] args) {
+		
+		//I am given (PQ, E) - I can encrypt anything with this
+		BigInteger PQ = new BigInteger("608485549753");
+		BigInteger E = new BigInteger("7");
+		
+		//Find P and Q - Both prime numbers
+		BigInteger P = new BigInteger("2");
+		BigInteger Q = null;
+		boolean foundP = false;
+		while(!foundP) {
+			if(PQ.mod(P) == BigInteger.ZERO) {
+				Q = (PQ.divide(P));
+				foundP = true;
+			}
+			else {
+				P = P.add(BigInteger.ONE);
+			}
+		}
+		//At this point, we have found P and Q
+		System.out.println("P: " + P + "\nQ: " + Q);
+		
+		//Find PhiPQ
+		BigInteger PhiPQ = (P.subtract(BigInteger.ONE)).multiply(Q.subtract(BigInteger.ONE));
+		System.out.println("PhiPQ: " + PhiPQ);
+		
+		//Find D
+		BigInteger D = E.modInverse(PhiPQ);
+		System.out.println("D: " + D);
+		
+		//Decrypt the message now
+		ArrayList<BigInteger> hiddenMessage = new ArrayList<BigInteger>();
+		String decodedMessage = "";
+		hiddenMessage.add(new BigInteger("576322461849"));
+		hiddenMessage.add(new BigInteger("122442824098"));
+		hiddenMessage.add(new BigInteger("34359738368"));
+		hiddenMessage.add(new BigInteger("29647771149"));
+		hiddenMessage.add(new BigInteger("140835578744"));
+		hiddenMessage.add(new BigInteger("546448062804"));
+		hiddenMessage.add(new BigInteger("120078454173"));
+		hiddenMessage.add(new BigInteger("42618442977"));
+		
+		for(BigInteger i : hiddenMessage) {
+			BigInteger m = i.modPow(D, PQ);
+			decodedMessage += (char)(m.intValue());
+		}
+		System.out.println(decodedMessage);
+	}
+
+}
